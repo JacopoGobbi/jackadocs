05bcdd0538c24044025ae10b688e3069f6cdbb66
Matthias Deja <MadocDoyu@gmail.com>
2018-05-02 18:55:45 +0200

    v0.1.0

--- docs-generator/src/main/scala/net/jackadull/jackadocs/docs/Main.scala
+++ docs-generator/src/main/scala/net/jackadull/jackadocs/docs/Main.scala
@@ -3,7 +3,6 @@ package net.jackadull.jackadocs.docs
 import net.jackadull.jackadocs.JackadocsInfo
 import net.jackadull.jackadocs.docs.readme.ReadmeRoot
 import net.jackadull.jackadocs.execution.Jackadocs
-import net.jackadull.jackadocs.rendering.{ChapterNumbering, RenderAsMarkdown}
 
 import scala.language.postfixOps
 
@@ -13,6 +12,4 @@ object Main extends App {
   jackadocs.requirePOMVersion("../pom.xml")(JackadocsInfo Version)
 
   jackadocs generateAt "../README.md" markdownFor ReadmeRoot
-
-  RenderAsMarkdown(ReadmeRoot, ChapterNumbering.empty) foreach {md ⇒ println(md.treeStructure())}
 }
